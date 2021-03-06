290b01ebac17fcfde0ae0db1419573c659900ea2
particleofmass <72720652+particleofmass@users.noreply.github.com>
2021-03-06 21:03:47 +0530

    Add files via upload

---
+++ brightness_py/amdgpu/brightness_down.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# Reading the current level of brightness using read mode
+f = open("/sys/class/backlight/amdgpu_bl0/brightness", "r")
+
+# bl stores the current brightness level
+bl = int(f.read())
+
+# Closing the file
+f.close()
+
+# Opens the file that adjusts brightness in write mode to overwrite the existing value
+f = open("/sys/class/backlight/amdgpu_bl0/brightness", "w")
+
+# increase brightness by 5
+bl -= 5
+
+# Replace the old brightness level with the new one
+f.write(str(bl))
+
+f.close()
+
+
+
---
+++ brightness_py/intelgpu/brightness_down.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# Reading the current level of brightness using read mode
+f = open("/sys/class/backlight/intel_backlight/brightness", "r")
+
+# bl stores the current brightness level
+bl = int(f.read())
+
+# Closing the file
+f.close()
+
+# Opens the file that adjusts brightness in write mode to overwrite the existing value
+f = open("/sys/class/backlight/intel_backlight/brightness", "w")
+
+# increase brightness by 5
+bl -= 5
+
+# Replace the old brightness level with the new one
+f.write(str(bl))
+
+f.close()
+
+
+
